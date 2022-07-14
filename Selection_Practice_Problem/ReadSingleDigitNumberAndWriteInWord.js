af6329f8ac99b16c29ac60672ae44991eb3eedf2
Inamul <inamulhoque097@gmail.com>
2022-07-14 14:35:56 +0530

    Added: Read Single Digit Number And Write In Word

---
+++ Selection_Practice_Problem/ReadSingleDigitNumberAndWriteInWord.js
@@ -0,0 +1,22 @@
+let num = Math.floor(Math.random()*10);
+if (num == 0) {
+    console.log(num+" -> Zero");
+} else if(num ==1){
+    console.log(num+" -> One");
+} else if(num ==2){
+    console.log(num+" -> Two");
+} else if(num ==3){
+    console.log(num+" -> Three");
+} else if(num ==4){
+    console.log(num+" -> Four");
+} else if(num ==5){
+    console.log(num+" -> Five");
+} else if(num ==6){
+    console.log(num+" -> Six");
+} else if(num ==7){
+    console.log(num+" -> Seven");
+} else if(num ==8){
+    console.log(num+" -> Eight");
+} else if(num ==9){
+    console.log(num+" -> Nine");
+}
