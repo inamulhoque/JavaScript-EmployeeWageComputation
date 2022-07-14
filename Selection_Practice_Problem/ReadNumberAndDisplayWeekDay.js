770c397fcf5983266514542c68455b354ec69389
Inamul <inamulhoque097@gmail.com>
2022-07-14 14:39:49 +0530

    Added: Read number and display corresponding week day

---
+++ Selection_Practice_Problem/ReadNumberAndDisplayWeekDay.js
@@ -0,0 +1,16 @@
+let num = Math.floor(Math.random()*6);
+if (num == 0) {
+    console.log(num+" -> Sunday");
+} else if(num ==1){
+    console.log(num+" -> Monday");
+} else if(num ==2){
+    console.log(num+" -> Tuesday");
+} else if(num ==3){
+    console.log(num+" -> Wednesday");
+} else if(num ==4){
+    console.log(num+" -> Thursday");
+} else if(num ==5){
+    console.log(num+" -> Friday");
+} else if(num ==6){
+    console.log(num+" -> Saturday");
+}
